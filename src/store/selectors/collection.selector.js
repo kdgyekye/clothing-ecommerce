962454e8e9570f3f54d9bac7073aab79c972bc52
kdgyekye <kingsleygyekye@gmail.com>
2021-06-14 23:33:11 +0000

    Changed store to persist only the cart items

--- src/store/selectors/collection.selector.js
+++ src/store/selectors/collection.selector.js
@@ -17,7 +17,7 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
     [selectCollections],
-    collections => Object.keys(collections).map(key => collections[key])
+    collections => collections? Object.keys(collections).map(key => collections[key]): []
 )
 
 export const selectCategories = categoryId => createSelector(
