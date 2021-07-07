0daf1cb6a10b431743ef909b3a989b3ef41513e8
kdgyekye <kingsleygyekye@gmail.com>
2021-07-07 00:52:53 +0000

    Replaced functional components with class components that use hooks

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -10,98 +10,77 @@ import * as Yup from 'yup'
 
 import './sign-in.style.scss';
 
-class SignIn extends React.Component {
-    constructor(props) {
-        super(props);
+const SignIn = (props) => {
 
-        this.state = {
-            email: '',
-            password: '',
-        };
-    }
-
-    handleSubmit = async event => {
+    const handleSubmit = async event => {
         event.preventDefault();
 
-        const {email,password} = this.props.values
+        const {email,password} = props.values
 
         try{
             await auth.signInWithEmailAndPassword(email,password)
-            this.setState({ email: '', password: '' });
         }catch (e) {
             console.log('Error: ',e)
             switch (e.code) {
                 case "auth/user-not-found":
-                    console.log('errors: ',this.props.errors['email'])
                     this.props.errors['email'] = 'Username/email is incorrect'
+                    console.log('errors: ',(this.props.errors['email']))
                     break
                 case "auth/wrong-password":
                     this.props.errors['password'] = 'Username/email is incorrect'
+                    console.log('errorsPassword: ',(this.props.errors['password']))
+                    break
+                default:
+                    this.props.errors['email'] = e.message
+                    console.log('errors: ',(this.props.errors['email']))
+                    break
             }
         }
     };
+    return (
+        <div className='sign-in'>
+            <h2>SIGN IN</h2>
+            <span>Sign in with your email and password</span>
 
-    handleChange = event => {
-        const { value, name } = event.target;
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    name='email'
+                    type='email'
+                    handleChange={props.handleChange}
+                    value={props.values['email']}
+                    label='Email'
+                    required
+                    onChange={props.handleChange}
+                    onBlur = {props.handleBlur}
+                    touched = {(props.touched['email'])}
+                    errors = {(props.errors['email'])}
 
-        this.setState({ [name]: value });
-    };
+                />
+                <FormInput
+                    name='password'
+                    type='password'
+                    value={props.values['password']}
+                    handleChange={props.handleChange}
+                    label='Password'
+                    required
+                    onChange={props.handleChange}
+                    onBlur = {props.handleBlur}
+                    touched = {(props.touched['password'])}
+                    errors = {(props.errors['password'])}
 
-    clearErrors = () => {
-        this.setState({emailError: '', passwordError: ''})
-    }
-    errorStyle = {
-        marginTop: '20px',
-        paddingLeft: '20px',
-        color: 'red'
-    }
-
-    render() {
-        return (
-            <div className='sign-in'>
-                <h2>SIGN IN</h2>
-                <span>Sign in with your email and password</span>
-
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput
-                        name='email'
-                        type='email'
-                        handleChange={this.props.handleChange}
-                        value={this.props.values['email']}
-                        label='Email'
-                        required
-                        onChange={this.props.handleChange}
-                        onBlur = {this.props.handleBlur}
-                        touched = {(this.props.touched['email'])}
-                        errors = {(this.props.errors['email'])}
-
-                    />
-                    <FormInput
-                        name='password'
-                        type='password'
-                        value={this.props.values['password']}
-                        handleChange={this.props.handleChange}
-                        label='Password'
-                        required
-                        onChange={this.props.handleChange}
-                        onBlur = {this.props.handleBlur}
-                        touched = {(this.props.touched['password'])}
-                        errors = {(this.props.errors['password'])}
-
-                    />
-                    <div className='row'>
-                        <div className='col-lg-5'>
-                            <CustomButton type='submit'> Sign in </CustomButton>
-                        </div>
-                        <div className='col-lg-7'>
-                            <CustomButton onClick={signInWithGoogle} type='button' isGoogleSignin> Sign in With Google</CustomButton>
-                        </div>
+                />
+                <div className='row'>
+                    <div className='col-lg-5'>
+                        <CustomButton type='submit'> Sign in </CustomButton>
                     </div>
-                    <div onClick={e => this.props.toggleComponent(false)} style={{cursor: 'pointer'}} className='text-decoration-underline sign-up-text'><p>Don't have an account, Sign Up here</p></div>
-                </form>
-            </div>
-        );
-    }
+                    <div className='col-lg-7 overflow-hidden'>
+                        <CustomButton onClick={signInWithGoogle} type='button' isGoogleSignin> Sign in With Google</CustomButton>
+                    </div>
+                </div>
+                <div onClick={e => props.toggleComponent(false)} style={{cursor: 'pointer'}} className='text-decoration-underline sign-up-text'><p>Don't have an account, Sign Up here</p></div>
+            </form>
+        </div>
+    );
 }
 
 export default withFormik({
--- src/pages/sign-in-and-up/sign-in-and-up.component.jsx
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -1,51 +1,41 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 
 import './sign-in-and-up.style.scss'
 import SignIn from "../../components/sign-in/sign-in.component";
 import SignUp from "../../components/sign-up/sign-up.component";
 import {Link} from "react-router-dom";
 
-class SignInAndOut extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            componentType: true
-        }
+const SignInAndOut = () => {
+    const [componentType, setComponentType] = useState(true)
+    const handleComponentChange = (toggle) => {
+        setComponentType(toggle)
     }
-
-    handleComponentChange = (toggle) => {
-        this.setState({componentType: toggle})
-    }
-    render() {
-        const {componentType} = this.state
-        return (
-            <div className='auth-page row'>
-                <div className='col-lg-5 col-sm-12'>
-                    <div className='sign-in-and-up'>
-                        <div className='logo-container' >
-                            <Link to={'/'}><h4>Unicorn Clothing</h4></Link>
-                            <span><img src='favicon.ico' alt='logo' className='logo'/></span>
-                        </div>
-                        <div className='container'>
-                            <div className=' col-sm-12'>
-                                <div className='mt-5 pt-5'>
-                                    {componentType?
-                                        <SignIn toggleComponent={this.handleComponentChange}/>
-                                        :
-                                        <SignUp toggleComponent={this.handleComponentChange}/>
-                                    }
-                                </div>
+    return (
+        <div className='auth-page row'>
+            <div className='col-lg-5 col-sm-12'>
+                <div className='sign-in-and-up'>
+                    <div className='logo-container' >
+                        <Link to={'/'}><h4>Unicorn Clothing</h4></Link>
+                        <span><img src='favicon.ico' alt='logo' className='logo'/></span>
+                    </div>
+                    <div className='container'>
+                        <div className='col-sm-12'>
+                            <div className='mt-5 pt-5'>
+                                {componentType?
+                                    <SignIn toggleComponent={handleComponentChange}/>
+                                    :
+                                    <SignUp toggleComponent={handleComponentChange}/>
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className='col-lg-7 col-sm-12'>
-                    <div className='auth-image' />
-                </div>
             </div>
-        )
-    }
+            <div className='col-lg-7 col-sm-12'>
+                <div className='auth-image' />
+            </div>
+        </div>
+    )
 }
 
 export default SignInAndOut
