b707af8bd7d2623781d7f0f744880b8b88ba5428
kdgyekye <kingsleygyekye@gmail.com>
2021-07-29 01:01:11 +0000

    Modified responsiveness

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -10,12 +10,13 @@ const CollectionPreview  = ({title, items}) => (
         <div className='collection-preview'>
             <h1 className='title'><Link to={`shop/${title.toLowerCase()}`}>{title}</Link></h1>
             <div className='preview'>
-                {
-                    items.filter((item,index) => index < 4).map((item) => {
-                        return (
-                            <CollectionItem key={item.id} item={item}/>
-                        )
-                    })}
+                    {
+                        items.filter((item,index) => index < 4).map((item) => {
+                            return (
+                                <CollectionItem key={item.id} item={item}/>
+                            )
+                        })
+                    }
             </div>
         </div>
 )
