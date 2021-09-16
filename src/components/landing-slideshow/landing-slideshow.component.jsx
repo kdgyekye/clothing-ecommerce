c9bd87e8b3db78c19ce2fae67e993e9be9346a35
kdgyekye <kingsleygyekye@gmail.com>
2021-09-16 22:22:56 +0000

    Style fixes

--- src/components/landing-slideshow/landing-slideshow.component.jsx
+++ src/components/landing-slideshow/landing-slideshow.component.jsx
@@ -7,18 +7,20 @@ const LandingSlideshow = (props) => {
     return (
         <Fragment>
             <div className='container-fluid'>
-                <ul className="slideshow cb-slideshow">
-                    <li><span>Image 01</span></li>
-                    <li><span>Image 02</span></li>
-                    <li><span>Image 03</span></li>
-                    <li><span>Image 04</span></li>
-                    <li><span>Image 05</span></li>
-                    <li><span>Image 06</span></li>
-                    <li><span>Image 07</span></li>
-                    <li><span>Image 08</span></li>
-                    <li><span>Image 09</span></li>
-                    <li><span>Image 10</span></li>
-                </ul>
+                <div className='hero bg-image'>
+                    <ul className="cb-slideshow">
+                        <li><span>Image 01</span></li>
+                        <li><span>Image 02</span></li>
+                        <li><span>Image 03</span></li>
+                        <li><span>Image 04</span></li>
+                        <li><span>Image 05</span></li>
+                        <li><span>Image 06</span></li>
+                        <li><span>Image 07</span></li>
+                        <li><span>Image 08</span></li>
+                        <li><span>Image 09</span></li>
+                        <li><span>Image 10</span></li>
+                    </ul>
+                </div>
             </div>
         </Fragment>
         )
