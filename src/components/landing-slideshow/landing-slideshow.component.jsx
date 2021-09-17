f144cafe75cfa56187d6cdd7bd8429ade67b293d
kdgyekye <kingsleygyekye@gmail.com>
2021-09-17 01:48:23 +0000

    Added typing effect to welcome customers

--- src/components/landing-slideshow/landing-slideshow.component.jsx
+++ src/components/landing-slideshow/landing-slideshow.component.jsx
@@ -1,9 +1,32 @@
 import React , {Fragment} from "react";
 
+import Typed from "typed.js";
+
 import './landing-slideshow.styles.scss'
 import '../../assets/slideshow.css'
 
 const LandingSlideshow = (props) => {
+
+    const el = React.useRef(null);
+    // Create reference to store the Typed instance itself
+    const typed = React.useRef(null);
+
+    React.useEffect(() => {
+        const options = {
+            strings: [
+                'Welcome To Our Store',
+            ],
+            typeSpeed: 50,
+            backSpeed: 50,
+        };
+
+        // elRef refers to the <span> rendered below
+        typed.current = new Typed(el.current, options);
+
+        return () => {
+            typed.current.destroy();
+        }
+    }, [])
     return (
         <Fragment>
             <div className='container-fluid'>
@@ -24,9 +47,8 @@ const LandingSlideshow = (props) => {
                     <div className="hero-content display-table">
                         <div className="table-cell">
                             <div className="container">
-                                <h1 className="hero-title mb-4">Welcome To Our Store</h1>
-                                <p className="hero-subtitle"><span className="typed" id= 'typing-stuff' data-typed-items="HAPPY, BIRTHDAY!!, :) "/>
-                                </p>
+                                <h1 className="hero-title mb-4"><span ref={el} /></h1>
+                                <button className='btn btn-lg shop-now'>SHOP NOW</button>
                             </div>
                         </div>
                     </div>
